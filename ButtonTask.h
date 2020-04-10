284273ee11b93f9389dc20aa80f1e7dd63fbb48e
xiaofan <xfan1024@live.com>
2020-04-11 02:52:45 +0800

    change task source to header only and avoid to use new operator
    Signed-off-by: xiaofan <xfan1024@live.com>

--- ButtonTask.h
+++ ButtonTask.h
@@ -1,19 +1,194 @@
 #ifndef __buttontask_h__
 #define __buttontask_h__
 
-#include "task.h"
 #include <functional>
+#include "common.h"
+#include "task.h"
+
+namespace buttontask_detail
+{
+
+static constexpr long debouncing_time = 30;
+static constexpr long double_click_interval_max = 300;
+static constexpr long long_press_min = 3000;
+
+enum
+{
+    S_unpressed_free,
+    S_pressed_debouncing,
+    S_pressed_confirmed,
+    S_unpressed_wait_next,
+    S_pressed_wait_unpressed,
+    S_max
+};
+
+static const char *str_state(unsigned int state)
+{
+    static const char *strtab[S_max] =
+    {
+        "unpressed_free",
+        "pressed_debouncing",
+        "pressed_confirmed",
+        "unpressed_wait_next",
+        "pressed_wait_unpressed",
+    };
+    if (state >= S_max)
+        return "unknown";
+    return strtab[state];
+}
+
+class ButtonImpl : public TaskImpl
+{
+public:
+    typedef std::function<void()> click_callback_type;
+    ButtonImpl(int pin, int pressed_level, int pin_mode)
+    {
+        m_pin = pin;
+        m_pin_mode = pin_mode;
+        m_pressed_level = pressed_level;
+    }
+
+    void on_click_callback(const click_callback_type& cb)
+    {
+        m_click_cb = cb;
+    }
+
+    void on_double_click_callback(const click_callback_type& cb)
+    {
+        m_double_click_cb = cb;
+    }
+
+    void on_long_press_callback(const click_callback_type& cb)
+    {
+        m_long_press_cb = cb;
+    }
+
+protected:
+    void on_start() override
+    {
+        m_state = S_unpressed_free;
+        pinMode(m_pin, m_pin_mode);
+    }
+
+    void on_loop() override
+    {
+        unsigned long current = millis();
+        unsigned long elapsed = current - m_privous_time;
+        bool pressed = is_pressed();
+        switch (m_state)
+        {
+        case S_unpressed_free:
+            if (!pressed)
+                return;
+            change_state(S_pressed_debouncing);
+            m_privous_time = current;
+            break;
+        case S_pressed_debouncing:
+            if (elapsed < debouncing_time)
+                return;
+            change_state(pressed ? S_pressed_confirmed : S_unpressed_free);
+            break;
+        case S_pressed_confirmed:
+            if (pressed)
+            {
+                if (elapsed > long_press_min)
+                {
+                    if (m_long_press_cb)
+                        m_long_press_cb();
+                    change_state(S_pressed_wait_unpressed);
+                }
+                return;
+            }
+            change_state(S_unpressed_wait_next);
+            m_privous_time = current;
+            break;
+        case S_unpressed_wait_next:
+            if (pressed && elapsed <= double_click_interval_max)
+            {
+                logv() << "double-click interval: " << elapsed << "ms";
+                change_state(S_pressed_wait_unpressed);
+                if (m_double_click_cb)
+                {
+                    logd() << "trigger double-click event";
+                    m_double_click_cb();
+                }
+            }
+            else if (!pressed && elapsed > double_click_interval_max)
+            {
+                change_state(S_unpressed_free);
+                if (m_click_cb)
+                {
+                    logd() << "trigger click event";
+                    m_click_cb();
+                }
+            }
+            break;
+        case S_pressed_wait_unpressed:
+            if (!pressed)
+                change_state(S_unpressed_free);
+            break;
+
+        }
+
+    }
+
+    void on_stop() override
+    {
+    }
+
+    const char* get_name() const override
+    {
+        return "Button";
+    }
+
+private:
+    bool is_pressed()
+    {
+        return m_pressed_level == digitalRead(m_pin);
+    }
+
+    void change_state(unsigned int state)
+    {
+        logv() << "state change from " << m_state << "(" << str_state(m_state) << ") to " << state << "(" << str_state(state) << ")";
+        m_state = state;
+    }
+
+    int m_pin;
+    int m_pin_mode;
+    int m_pressed_level;
+    unsigned long m_privous_time;
+    unsigned int m_state;
+    click_callback_type m_click_cb;
+    click_callback_type m_double_click_cb;
+    click_callback_type m_long_press_cb;
+};
+
+}
 
 class ButtonTask : public Task
 {
 public:
-    typedef std::function<void()> click_callback_type;
-    ButtonTask(int pin, int pressed_level, int pin_mode = INPUT);
-    ~ButtonTask();
+    typedef buttontask_detail::ButtonImpl::click_callback_type click_callback_type;
 
-    void on_click_callback(const click_callback_type& cb);
-    void on_double_click_callback(const click_callback_type& cb);
-    void on_long_press_callback(const click_callback_type &cb);
+    ButtonTask(int pin, int pressed_level, int pin_mode = INPUT) : m_impl(pin, pressed_level, pin_mode), Task(&m_impl)
+    {}
+
+    void on_click_callback(const click_callback_type& cb)
+    {
+        m_impl.on_click_callback(cb);
+    }
+
+    void on_double_click_callback(const click_callback_type& cb)
+    {
+        m_impl.on_double_click_callback(cb);
+    }
+
+    void on_long_press_callback(const click_callback_type& cb)
+    {
+        m_impl.on_long_press_callback(cb);
+    }
+private:
+    buttontask_detail::ButtonImpl m_impl;
 };
 
 #endif
--- ConfigTask.h
+++ ConfigTask.h
@@ -2,14 +2,163 @@
 #define __discoverytask_h__
 
 #include <functional>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#include "common.h"
 #include "task.h"
+
+namespace configtask_detail
+{
+
+static constexpr uint16_t config_udp_port = 7866;
+typedef StaticJsonDocument<256> JsonDocumentType;
+
+enum
+{
+    CONFERR_DONOT_REPLY = -10000,
+    CONFERR_OK = 0,
+    CONFERR_FAIL,
+    CONFERR_INVALID,
+    CONFERR_NO_COMMAND,
+};
+
+class ConfigImpl : public TaskImpl {
+public:
+    void on_config_wifi(const std::function<void(const char *ssid, const char *key)> &cb)
+    {
+        m_config_wifi_cb = cb;
+    }
+
+protected:
+    void on_start() override
+    {
+        m_udp.begin(config_udp_port);
+    }
+
+    void on_loop() override
+    {
+        char buffer[256];
+        JsonDocumentType json;
+        if (m_udp.parsePacket() == 0)
+            return;
+
+        size_t total = m_udp.readBytes((uint8_t*)buffer, sizeof(buffer) - 1);
+        buffer[total] = 0;
+        logv() << "received config packet(len=" << total << ") from " << m_udp.remoteIP() << ": " << buffer;
+        if (deserializeJson(json, buffer))
+        {
+            logd() << "parse json fail";
+            return;
+        }
+
+        int id = 0;
+        bool id_exist = true;
+
+        if (!json["method"].is<String>())
+            return;
+
+        if (json["id"].is<int>())
+            id = json["id"];
+        else
+            id_exist = false;
+
+        String str = json["method"].as<String>();
+        int code = CONFERR_NO_COMMAND;
+
+        if (str == "discovery")
+            code = do_discovery(json);
+        else if (str == "config_wifi")
+            code = do_config_wifi(json);
+
+        if (code != CONFERR_DONOT_REPLY)
+        {
+            if (code != CONFERR_OK)
+                json.clear();
+            if (id_exist)
+                json["id"] = id;
+            else
+                json["id"] = nullptr;
+            json["result"] = code;
+            serializeJson(json, str);
+            logv() << "reply: " << str;
+            m_udp.beginPacket(m_udp.remoteIP(), m_udp.remotePort());
+            m_udp.write(str.c_str(), str.length());
+            m_udp.endPacket();
+        }
+    }
+
+    void on_stop() override
+    {
+        m_udp.stop();
+    }
+
+    const char* get_name() const override
+    {
+        return "Config";
+    }
+private:
+    int do_discovery(JsonDocumentType& json)
+    {
+        if (json["device_type"].is<String>() && json["device_type"].as<String>() != get_device_type())
+            return CONFERR_DONOT_REPLY;
+
+        if (json["device_id"].is<String>() && json["device_id"].as<String>() != get_device_id())
+            return CONFERR_DONOT_REPLY;
+
+        json.clear();
+        json["device_type"] = get_device_type();
+        json["device_id"] = get_device_id();
+        return CONFERR_OK;
+    }
+
+    int do_config_wifi(JsonDocumentType& json)
+    {
+        String ssid, key;
+        bool key_exist = false;
+        if (!json["ssid"].is<String>())
+        {
+            logw() << "require ssid";
+            return CONFERR_INVALID;
+        }
+
+        if (json["key"].is<String>())
+            key_exist = true;
+
+        ssid = json["ssid"].as<String>();
+        if (key_exist)
+        {
+            key = json["key"].as<String>();
+            if (key.length() < 8)
+            {
+                logw() << "key is too short";
+                return CONFERR_INVALID;
+            }
+        }
+        if (m_config_wifi_cb)
+            m_config_wifi_cb(ssid.c_str(), key_exist ? key.c_str() : nullptr);
+        json.clear();
+        return CONFERR_OK;
+    }
+
+    WiFiUDP m_udp;
+    std::function<void(const char *name, const char *key)> m_config_wifi_cb;
+};
+
+}
 
 class ConfigTask : public Task
 {
 public:
-    void on_config_wifi(const std::function<void(const char *ssid, const char *key)> &cb);
-    ConfigTask();
-    ~ConfigTask();
+    void on_config_wifi(const std::function<void(const char *ssid, const char *key)> &cb)
+    {
+        m_impl.on_config_wifi(cb);
+    }
+
+    ConfigTask() : Task(&m_impl)
+    {}
+private:
+    configtask_detail::ConfigImpl m_impl;
 };
 
 #endif
